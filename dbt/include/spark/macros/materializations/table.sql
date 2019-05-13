a91859946cba2cb32b7358ffe592805841ae1a2b
Jeremy Cohen <jeremy@fishtownanalytics.com>
2019-05-13 16:35:29 -0400

    Rm superfluous

--- dbt/include/spark/macros/materializations/table.sql
+++ dbt/include/spark/macros/materializations/table.sql
@@ -1,19 +1,11 @@
 {% materialization table, adapter = 'spark' %}
   {%- set identifier = model['alias'] -%}
 
-  {%- set non_destructive_mode = (flags.NON_DESTRUCTIVE == True) -%}
-  {% if non_destructive_mode %}
-    {{ exceptions.raise_compiler_error("--non-destructive mode is not supported on Spark") }}
-  {% endif %}
-
   {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
   {%- set target_relation = api.Relation.create(identifier=identifier,
                                                 schema=schema,
                                                 database=database,
                                                 type='table') -%}
-
-  {%- set exists_as_table = (old_relation is not none and old_relation.is_table) -%}
-  {%- set exists_as_view = (old_relation is not none and old_relation.is_view) -%}
 
   -- setup: if the target relation already exists, drop it
   {% if old_relation -%}
