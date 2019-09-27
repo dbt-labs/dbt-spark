37ea75618d225cd9071f1c8752fbe2db922797f3
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2019-09-27 15:10:15 -0700

    added

--- dbt/include/spark/macros/adapters.sql
+++ dbt/include/spark/macros/adapters.sql
@@ -111,3 +111,40 @@
     drop {{ type }} if exists {{ relation }}
   {%- endcall %}
 {% endmacro %}
+
+{% macro spark__load_csv_rows(model, batch_size, agate_table) %}
+    {% set cols_sql = ", ".join(agate_table.column_names) %}
+    {% set bindings = [] %}
+
+    {% set statements = [] %}
+
+    {% for chunk in agate_table.rows | batch(batch_size) %}
+        {% set bindings = [] %}
+
+        {% for row in chunk %}
+            {% set _ = bindings.extend(row) %}
+        {% endfor %}
+
+        {% set sql %}
+            insert into {{ this.render(False) }}
+            /* Expected column list: ({{ cols_sql }}) */
+            values
+            {% for row in chunk -%}
+                ({%- for column in agate_table.column_names -%}
+                    %s
+                    {%- if not loop.last%},{%- endif %}
+                {%- endfor -%})
+                {%- if not loop.last%},{%- endif %}
+            {%- endfor %}
+        {% endset %}
+
+        {% set _ = adapter.add_query(sql, bindings=bindings, abridge_sql_log=True) %}
+
+        {% if loop.index0 == 0 %}
+            {% set _ = statements.append(sql) %}
+        {% endif %}
+    {% endfor %}
+
+    {# Return SQL so we can render it out into the compiled files #}
+    {{ return(statements[0]) }}
+{% endmacro %}
