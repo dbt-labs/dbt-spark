32cdb40886699416a015ea48ebfd15edae49c7e7
Tony Qiu <tqiu@kabbage.com>
2020-03-03 09:58:26 -0500

    Fix the seed bug

--- dbt/include/spark/macros/materializations/seed.sql
+++ dbt/include/spark/macros/materializations/seed.sql
@@ -32,3 +32,54 @@
     {# Return SQL so we can render it out into the compiled files #}
     {{ return(statements[0]) }}
 {% endmacro %}
+
+
+{% materialization seed, adapter='spark' %}
+
+  {%- set identifier = model['alias'] -%}
+  {%- set full_refresh_mode = (flags.FULL_REFRESH == True) -%}
+
+  {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
+
+  {%- set exists_as_table = (old_relation is not none and get_relation_type(old_relation) == 'table') -%}
+  {%- set exists_as_view = (old_relation is not none and get_relation_type(old_relation) == 'view') -%}
+
+  {%- set agate_table = load_agate_table() -%}
+  {%- do store_result('agate_table', status='OK', agate_table=agate_table) -%}
+
+  {{ run_hooks(pre_hooks, inside_transaction=False) }}
+
+  -- `BEGIN` happens here:
+  {{ run_hooks(pre_hooks, inside_transaction=True) }}
+
+  -- build model
+  {% set create_table_sql = "" %}
+  {% if exists_as_view %}
+    {{ exceptions.raise_compiler_error("Cannot seed to '{}', it is a view".format(old_relation)) }}
+  {% elif exists_as_table %}
+    {% set create_table_sql = reset_csv_table(model, full_refresh_mode, old_relation, agate_table) %}
+  {% else %}
+    {% set create_table_sql = create_csv_table(model, agate_table) %}
+  {% endif %}
+
+  {% set status = 'CREATE' if full_refresh_mode else 'INSERT' %}
+  {% set num_rows = (agate_table.rows | length) %}
+  {% set sql = load_csv_rows(model, agate_table) %}
+
+  {% call noop_statement('main', status ~ ' ' ~ num_rows) %}
+    {{ create_table_sql }};
+    -- dbt seed --
+    {{ sql }}
+  {% endcall %}
+
+  {{ run_hooks(post_hooks, inside_transaction=True) }}
+
+  -- `COMMIT` happens here
+  {{ adapter.commit() }}
+
+  {{ run_hooks(post_hooks, inside_transaction=False) }}
+
+  {% set target_relation = this.incorporate(type='table') %}
+  {{ return({'relations': [target_relation]}) }}
+
+{% endmaterialization %}
