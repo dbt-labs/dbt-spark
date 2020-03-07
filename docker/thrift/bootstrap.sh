3444cb83a41f7371db40a1189cc2b560b5f41e92
Daniel Mateus Pires <dpires@earnestresearch.com>
2020-03-07 20:08:58 +0000

    Add consistent spark-warehouse

--- docker/thrift/bootstrap.sh
+++ docker/thrift/bootstrap.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 set -e  # Fail script on error
+
 mkdir -p /tmp/spark-events
-sbin/start-history-server.sh
+
+echo "Waiting for hive-metastore to be ready.."
+while !</dev/tcp/hive-metastore/5432
+do
+    echo "hive-metastore was not ready yet.."
+    sleep 2
+done
+
+echo "starting Spark Thrift Server.."
 sbin/start-thriftserver.sh
+
+echo "Creating default database (if not exists) ${DEFAULT_DATABASE}"
+bin/spark-sql -e "CREATE DATABASE IF NOT EXISTS ${DEFAULT_DATABASE}"
+
 tail -f logs/*
