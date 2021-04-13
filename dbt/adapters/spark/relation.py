07e4c5549896ddb9c3c82b938d29a9d11ff6cda2
Fran Lozano <francisco.lozano@netlify.com>
2021-04-13 23:28:26 +0200

    Parse information returned by show table extended

--- dbt/adapters/spark/relation.py
+++ dbt/adapters/spark/relation.py
@@ -26,6 +26,7 @@ class SparkRelation(BaseRelation):
     include_policy: SparkIncludePolicy = SparkIncludePolicy()
     quote_character: str = '`'
     is_delta: Optional[bool] = None
+    information: str = None
 
     def __post_init__(self):
         if self.database != self.schema and self.database:
