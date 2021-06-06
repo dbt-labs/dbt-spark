b1e5f77ac334968cf3ea30d2435c183e6c20bac0
Jeremy Cohen <jeremy@fishtownanalytics.com>
2021-06-06 15:17:25 -0400

    Explicitly cast column types when inserting into seeds (#166)
    * Explicitly cast column types when inserting into seeds
    
    * Add changelog entry

--- dbt/include/spark/macros/materializations/seed.sql
+++ dbt/include/spark/macros/materializations/seed.sql
@@ -1,6 +1,7 @@
 {% macro spark__load_csv_rows(model, agate_table) %}
     {% set batch_size = 1000 %}
-
+    {% set column_override = model['config'].get('column_types', {}) %}
+    
     {% set statements = [] %}
 
     {% for chunk in agate_table.rows | batch(batch_size) %}
@@ -13,12 +14,10 @@
         {% set sql %}
             insert into {{ this.render() }} values
             {% for row in chunk -%}
-                ({%- for column in agate_table.columns -%}
-                    {%- if 'ISODate' in (column.data_type | string) -%}
-                      cast(%s as timestamp)
-                    {%- else -%}
-                    %s
-                    {%- endif -%}
+                ({%- for col_name in agate_table.column_names -%}
+                    {%- set inferred_type = adapter.convert_type(agate_table, loop.index0) -%}
+                    {%- set type = column_override.get(col_name, inferred_type) -%}
+                      cast(%s as {{type}})
                     {%- if not loop.last%},{%- endif %}
                 {%- endfor -%})
                 {%- if not loop.last%},{%- endif %}
