2c355657a21160f4a66fac152f8c777a88aacb0b
u1214742 <u1214742@USNYCGL415TMD6R.local>
2021-06-21 23:51:23 -0400

    add truncate and drop with full refresh

--- dbt/include/spark/macros/materializations/seed.sql
+++ dbt/include/spark/macros/materializations/seed.sql
@@ -36,10 +36,14 @@
 {% endmacro %}
 
 {% macro spark__reset_csv_table(model, full_refresh, old_relation, agate_table) %}
-    {% if old_relation %}
+    {% set sql = "" %}
+    {% if full_refresh %}
         {{ adapter.drop_relation(old_relation) }}
+        {% set sql = create_csv_table(model, agate_table) %}
+    {% else %}
+        {{ adapter.truncate_relation(old_relation) }}
+        {% set sql = "truncate table " ~ old_relation %}
     {% endif %}
-    {% set sql = create_csv_table(model, agate_table) %}
     {{ return(sql) }}
 {% endmacro %}
 
