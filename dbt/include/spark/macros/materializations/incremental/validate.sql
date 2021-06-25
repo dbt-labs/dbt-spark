015c12c6f34f77f3373e592db6c19a1806e7c012
flvndh <17010377+flvndh@users.noreply.github.com>
2021-06-25 13:28:27 +0200

    Remove error when using 'delta' with 'insert_overwrite'

--- dbt/include/spark/macros/materializations/incremental/validate.sql
+++ dbt/include/spark/macros/materializations/incremental/validate.sql
@@ -29,12 +29,6 @@
     You can only choose this strategy when file_format is set to 'delta'
   {%- endset %}
   
-  {% set invalid_insert_overwrite_delta_msg -%}
-    Invalid incremental strategy provided: {{ raw_strategy }}
-    You cannot use this strategy when file_format is set to 'delta'
-    Use the 'append' or 'merge' strategy instead
-  {%- endset %}
-  
   {% set invalid_insert_overwrite_endpoint_msg -%}
     Invalid incremental strategy provided: {{ raw_strategy }}
     You cannot use this strategy when connecting via endpoint
@@ -46,9 +40,6 @@
   {%-else %}
     {% if raw_strategy == 'merge' and file_format != 'delta' %}
       {% do exceptions.raise_compiler_error(invalid_merge_msg) %}
-    {% endif %}
-    {% if raw_strategy == 'insert_overwrite' and file_format == 'delta' %}
-      {% do exceptions.raise_compiler_error(invalid_insert_overwrite_delta_msg) %}
     {% endif %}
     {% if raw_strategy == 'insert_overwrite' and target.endpoint %}
       {% do exceptions.raise_compiler_error(invalid_insert_overwrite_endpoint_msg) %}
