e46e4e2081990b1f7a6361e9e9d4d3b88001c1ad
Leah Antkiewicz <leah.antkiewicz@dbtlabs.com>
2021-08-20 19:26:29 +0000

    Bumping version to 0.21.0b2

--- setup.py
+++ setup.py
@@ -28,9 +28,9 @@ def _dbt_spark_version():
 package_version = _dbt_spark_version()
 description = """The SparkSQL plugin for dbt (data build tool)"""
 
-dbt_version = '0.21.0b1'
+dbt_version = '0.21.0b2'
 # the package version should be the dbt version, with maybe some things on the
-# ends of it. (0.21.0b1 vs 0.21.0b1a1, 0.21.0b1.1, ...)
+# ends of it. (0.21.0b2 vs 0.21.0b2a1, 0.21.0b2.1, ...)
 if not package_version.startswith(dbt_version):
     raise ValueError(
         f'Invalid setup.py: package_version={package_version} must start with '
