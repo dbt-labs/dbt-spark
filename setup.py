27e19f65aa1dfdc234822862a8db9e55bdd3d388
leahwicz <60146280+leahwicz@users.noreply.github.com>
2021-10-22 11:36:58 -0400

    Moving unit testing to Actions (#237)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import re
 
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md')) as f:
+with open(os.path.join(this_directory, 'README.md'), 'r', encoding='utf8') as f:
     long_description = f.read()
 
 
