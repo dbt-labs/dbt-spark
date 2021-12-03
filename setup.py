8b2f00a7e89058d676128388c81cbed729e436be
leahwicz <60146280+leahwicz@users.noreply.github.com>
2021-12-03 16:34:16 -0500

    Fix version for package

--- setup.py
+++ setup.py
@@ -55,9 +55,9 @@ def _get_dbt_core_version():
     pre = (parts["prekind"]+"1" if parts["prekind"] else "")
     return f"{minor}{pre}"
 
-
+# TODO remove old logic and add to versionBump script
 package_name = "dbt-spark"
-package_version = _get_plugin_version()
+package_version = "1.0.0"
 dbt_core_version = _get_dbt_core_version()
 description = """The Apache Spark adapter plugin for dbt"""
 
