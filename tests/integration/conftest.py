10267b15c3d509d3bfc5876968de42de1a28caf7
Cor Zuurmond <jczuurmond@protonmail.com>
2022-01-28 15:11:18 +0100

    Add fixutre to force use spark session

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,10 +1,14 @@
+import pytest
+from pyspark.sql import SparkSession
+
+
+@pytest.fixture(autouse=True)
+def spark(spark_session: SparkSession) -> SparkSession:
+    """Force use Spark session."""
+    return spark_session
+
+
 def pytest_configure(config):
-    config.addinivalue_line(
-        "markers", "profile_databricks_cluster"
-    )
-    config.addinivalue_line(
-        "markers", "profile_databricks_sql_endpoint"
-    )
-    config.addinivalue_line(
-        "markers", "profile_apache_spark"
-    )
+    config.addinivalue_line("markers", "profile_databricks_cluster")
+    config.addinivalue_line("markers", "profile_databricks_sql_endpoint")
+    config.addinivalue_line("markers", "profile_apache_spark")
