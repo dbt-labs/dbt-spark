4fb252cb46259a069164c2b9343ff9ab570a3e9a
Cor Zuurmond <jczuurmond@protonmail.com>
2022-02-09 10:07:51 +0100

    Remove spark fixture

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,13 +1,3 @@
-import pytest
-from pyspark.sql import SparkSession
-
-
-@pytest.fixture(autouse=True)
-def spark(spark_session: SparkSession) -> SparkSession:
-    """Force use Spark session."""
-    return spark_session
-
-
 def pytest_configure(config):
     config.addinivalue_line("markers", "profile_databricks_cluster")
     config.addinivalue_line("markers", "profile_databricks_sql_endpoint")
