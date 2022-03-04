1ca39e3cbe052eb0b16ec7e6540e35b665e19b36
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-03-04 10:44:24 -0600

    init push for spark pre-commit

--- dbt/include/spark/macros/materializations/incremental/strategies.sql
+++ dbt/include/spark/macros/materializations/incremental/strategies.sql
@@ -1,5 +1,5 @@
 {% macro get_insert_overwrite_sql(source_relation, target_relation) %}
-    
+
     {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
     {%- set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') -%}
     insert overwrite table {{ target_relation }}
@@ -22,7 +22,7 @@
 {% macro spark__get_merge_sql(target, source, unique_key, dest_columns, predicates=none) %}
   {# skip dest_columns, use merge_update_columns config if provided, otherwise use "*" #}
   {%- set update_columns = config.get("merge_update_columns") -%}
-  
+
   {% set merge_condition %}
     {% if unique_key %}
         on DBT_INTERNAL_SOURCE.{{ unique_key }} = DBT_INTERNAL_DEST.{{ unique_key }}
@@ -30,19 +30,19 @@
         on false
     {% endif %}
   {% endset %}
-  
+
     merge into {{ target }} as DBT_INTERNAL_DEST
       using {{ source.include(schema=false) }} as DBT_INTERNAL_SOURCE
-      
+
       {{ merge_condition }}
-      
+
       when matched then update set
         {% if update_columns -%}{%- for column_name in update_columns %}
             {{ column_name }} = DBT_INTERNAL_SOURCE.{{ column_name }}
             {%- if not loop.last %}, {%- endif %}
         {%- endfor %}
         {%- else %} * {% endif %}
-    
+
       when not matched then insert *
 {% endmacro %}
 
