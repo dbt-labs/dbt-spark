a8dc98a43b01b6421b86e23c2b41e7baed6234bd
Jeremy Cohen <jeremy@dbtlabs.com>
2022-03-11 14:33:49 -0500

    More type casting

--- tests/integration/incremental_unique_id_test/models/expected/unique_key_list__inplace_overwrite.sql
+++ tests/integration/incremental_unique_id_test/models/expected/unique_key_list__inplace_overwrite.sql
@@ -10,12 +10,12 @@ select
     'Hartford' as city,
     cast('2022-02-14' as date) as last_visit_date
 union all
-select 'MA','Suffolk','Boston','2020-02-12'
+select 'MA','Suffolk','Boston',cast('2020-02-12' as date)
 union all
-select 'NJ','Mercer','Trenton','2022-01-01'
+select 'NJ','Mercer','Trenton',cast('2022-01-01' as date)
 union all
-select 'NY','Kings','Brooklyn','2021-04-02'
+select 'NY','Kings','Brooklyn',cast('2021-04-02' as date)
 union all
-select 'NY','New York','Manhattan','2021-04-01'
+select 'NY','New York','Manhattan',cast('2021-04-01' as date)
 union all
-select 'PA','Philadelphia','Philadelphia','2021-05-21'
+select 'PA','Philadelphia','Philadelphia',cast('2021-05-21' as date)
