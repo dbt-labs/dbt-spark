36db37766b3616275a38d4ea9f7ad46b77629385
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-04-06 15:04:41 -0500

    remove old tests

--- tests/integration/incremental_unique_id_test/seeds/duplicate_insert.sql
+++
@@ -1,5 +0,0 @@
--- insert new row, which should not be in incremental model
---  with primary or first three columns unique
-insert into {schema}.seed
-    (state, county, city, last_visit_date)
-values ('CT','Hartford','Hartford',cast('2022-02-14' as date));
