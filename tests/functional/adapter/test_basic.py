5fbcc2faaaffa7ea022dc3f7e394a9b8bf98a5f8
Gerda Shank <gerda@dbtlabs.com>
2022-04-19 20:11:22 -0400

    Implement TestDocsGenerateSpark

--- tests/functional/adapter/test_basic.py
+++ tests/functional/adapter/test_basic.py
@@ -12,9 +12,12 @@ from dbt.tests.adapter.basic.test_generic_tests import BaseGenericTests
 from dbt.tests.adapter.basic.test_snapshot_check_cols import BaseSnapshotCheckCols
 from dbt.tests.adapter.basic.test_snapshot_timestamp import BaseSnapshotTimestamp
 from dbt.tests.adapter.basic.test_adapter_methods import BaseAdapterMethod
+from dbt.tests.adapter.basic.test_docs_generate import BaseDocsGenerate
+from dbt.tests.adapter.basic.expected_catalog import base_expected_catalog, no_stats
+from dbt.tests.util import AnyFloat
 
 
-@pytest.mark.skip_profile('spark_session')
+@pytest.mark.skip_profile("spark_session")
 class TestSimpleMaterializationsSpark(BaseSimpleMaterializations):
     pass
 
@@ -25,7 +28,7 @@ class TestSingularTestsSpark(BaseSingularTests):
 
 # The local cluster currently tests on spark 2.x, which does not support this
 # if we upgrade it to 3.x, we can enable this test
-@pytest.mark.skip_profile('apache_spark')
+@pytest.mark.skip_profile("apache_spark")
 class TestSingularTestsEphemeralSpark(BaseSingularTestsEphemeral):
     pass
 
@@ -34,12 +37,12 @@ class TestEmptySpark(BaseEmpty):
     pass
 
 
-@pytest.mark.skip_profile('spark_session')
+@pytest.mark.skip_profile("spark_session")
 class TestEphemeralSpark(BaseEphemeral):
     pass
 
 
-@pytest.mark.skip_profile('spark_session')
+@pytest.mark.skip_profile("spark_session")
 class TestIncrementalSpark(BaseIncremental):
     pass
 
@@ -50,7 +53,7 @@ class TestGenericTestsSpark(BaseGenericTests):
 
 # These tests were not enabled in the dbtspec files, so skipping here.
 # Error encountered was: Error running query: java.lang.ClassNotFoundException: delta.DefaultSource
-@pytest.mark.skip_profile('apache_spark', 'spark_session')
+@pytest.mark.skip_profile("apache_spark", "spark_session")
 class TestSnapshotCheckColsSpark(BaseSnapshotCheckCols):
     @pytest.fixture(scope="class")
     def project_config_update(self):
@@ -60,13 +63,13 @@ class TestSnapshotCheckColsSpark(BaseSnapshotCheckCols):
             },
             "snapshots": {
                 "+file_format": "delta",
-            }
+            },
         }
 
 
-#hese tests were not enabled in the dbtspec files, so skipping here.
+# hese tests were not enabled in the dbtspec files, so skipping here.
 # Error encountered was: Error running query: java.lang.ClassNotFoundException: delta.DefaultSource
-@pytest.mark.skip_profile('apache_spark', 'spark_session')
+@pytest.mark.skip_profile("apache_spark", "spark_session")
 class TestSnapshotTimestampSpark(BaseSnapshotTimestamp):
     @pytest.fixture(scope="class")
     def project_config_update(self):
@@ -76,8 +79,44 @@ class TestSnapshotTimestampSpark(BaseSnapshotTimestamp):
             },
             "snapshots": {
                 "+file_format": "delta",
-            }
+            },
         }
+
 
 class TestBaseAdapterMethod(BaseAdapterMethod):
     pass
+
+
+def spark_seed_stats():
+    return {
+        "bytes": {
+            "description": None,
+            "id": "bytes",
+            "include": True,
+            "label": "bytes",
+            "value": AnyFloat(),
+        },
+        "has_stats": {
+            "description": "Indicates whether there are statistics for this " "table",
+            "id": "has_stats",
+            "include": False,
+            "label": "Has Stats?",
+            "value": True,
+        },
+    }
+
+
+class TestDocsGenerateSpark(BaseDocsGenerate):
+    @pytest.fixture(scope="class")
+    def expected_catalog(self, project):
+        return base_expected_catalog(
+            project,
+            role="root",
+            id_type="long",
+            text_type="string",
+            time_type="timestamp",
+            view_type="view",
+            table_type="table",
+            model_stats=no_stats(),
+            seed_stats=spark_seed_stats(),
+        )
