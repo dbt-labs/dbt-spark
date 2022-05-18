beef079ef0d61147fcaef6d736b588843abf0fb0
Jeremy Cohen <jeremy@dbtlabs.com>
2022-05-18 12:33:08 +0200

    Fixups

--- dbt/include/spark/macros/utils/dateadd.sql
+++ dbt/include/spark/macros/utils/dateadd.sql
@@ -2,26 +2,26 @@
 
     {%- set clock_component -%}
         {# make sure the dates + timestamps are real, otherwise raise an error asap #}
-        to_unix_timestamp({{ spark_utils.assert_not_null('to_timestamp', from_date_or_timestamp) }})
-        - to_unix_timestamp({{ spark_utils.assert_not_null('date', from_date_or_timestamp) }})
+        to_unix_timestamp({{ assert_not_null('to_timestamp', from_date_or_timestamp) }})
+        - to_unix_timestamp({{ assert_not_null('date', from_date_or_timestamp) }})
     {%- endset -%}
 
     {%- if datepart in ['day', 'week'] -%}
-        
+
         {%- set multiplier = 7 if datepart == 'week' else 1 -%}
 
         to_timestamp(
             to_unix_timestamp(
                 date_add(
-                    {{ spark_utils.assert_not_null('date', from_date_or_timestamp) }},
+                    {{ assert_not_null('date', from_date_or_timestamp) }},
                     cast({{interval}} * {{multiplier}} as int)
                 )
             ) + {{clock_component}}
         )
 
     {%- elif datepart in ['month', 'quarter', 'year'] -%}
-    
-        {%- set multiplier -%} 
+
+        {%- set multiplier -%}
             {%- if datepart == 'month' -%} 1
             {%- elif datepart == 'quarter' -%} 3
             {%- elif datepart == 'year' -%} 12
@@ -31,15 +31,15 @@
         to_timestamp(
             to_unix_timestamp(
                 add_months(
-                    {{ spark_utils.assert_not_null('date', from_date_or_timestamp) }},
+                    {{ assert_not_null('date', from_date_or_timestamp) }},
                     cast({{interval}} * {{multiplier}} as int)
                 )
             ) + {{clock_component}}
         )
 
     {%- elif datepart in ('hour', 'minute', 'second', 'millisecond', 'microsecond') -%}
-    
-        {%- set multiplier -%} 
+
+        {%- set multiplier -%}
             {%- if datepart == 'hour' -%} 3600
             {%- elif datepart == 'minute' -%} 60
             {%- elif datepart == 'second' -%} 1
@@ -49,7 +49,7 @@
         {%- endset -%}
 
         to_timestamp(
-            {{ spark_utils.assert_not_null('to_unix_timestamp', from_date_or_timestamp) }}
+            {{ assert_not_null('to_unix_timestamp', from_date_or_timestamp) }}
             + cast({{interval}} * {{multiplier}} as int)
         )
 
--- dbt/include/spark/macros/utils/datediff.sql
+++ dbt/include/spark/macros/utils/datediff.sql
@@ -1,24 +1,24 @@
 {% macro spark__datediff(first_date, second_date, datepart) %}
 
     {%- if datepart in ['day', 'week', 'month', 'quarter', 'year'] -%}
-    
+
         {# make sure the dates are real, otherwise raise an error asap #}
-        {% set first_date = spark_utils.assert_not_null('date', first_date) %}
-        {% set second_date = spark_utils.assert_not_null('date', second_date) %}
-    
+        {% set first_date = assert_not_null('date', first_date) %}
+        {% set second_date = assert_not_null('date', second_date) %}
+
     {%- endif -%}
-    
+
     {%- if datepart == 'day' -%}
-    
+
         datediff({{second_date}}, {{first_date}})
-    
+
     {%- elif datepart == 'week' -%}
-    
+
         case when {{first_date}} < {{second_date}}
             then floor(datediff({{second_date}}, {{first_date}})/7)
             else ceil(datediff({{second_date}}, {{first_date}})/7)
             end
-        
+
         -- did we cross a week boundary (Sunday)?
         + case
             when {{first_date}} < {{second_date}} and dayofweek({{second_date}}) < dayofweek({{first_date}}) then 1
@@ -31,20 +31,20 @@
             then floor(months_between(date({{second_date}}), date({{first_date}})))
             else ceil(months_between(date({{second_date}}), date({{first_date}})))
             end
-        
+
         -- did we cross a month boundary?
         + case
             when {{first_date}} < {{second_date}} and dayofmonth({{second_date}}) < dayofmonth({{first_date}}) then 1
             when {{first_date}} > {{second_date}} and dayofmonth({{second_date}}) > dayofmonth({{first_date}}) then -1
             else 0 end
-        
+
     {%- elif datepart == 'quarter' -%}
-    
+
         case when {{first_date}} < {{second_date}}
             then floor(months_between(date({{second_date}}), date({{first_date}}))/3)
             else ceil(months_between(date({{second_date}}), date({{first_date}}))/3)
             end
-        
+
         -- did we cross a quarter boundary?
         + case
             when {{first_date}} < {{second_date}} and (
@@ -58,12 +58,12 @@
             else 0 end
 
     {%- elif datepart == 'year' -%}
-    
+
         year({{second_date}}) - year({{first_date}})
 
     {%- elif datepart in ('hour', 'minute', 'second', 'millisecond', 'microsecond') -%}
-    
-        {%- set divisor -%} 
+
+        {%- set divisor -%}
             {%- if datepart == 'hour' -%} 3600
             {%- elif datepart == 'minute' -%} 60
             {%- elif datepart == 'second' -%} 1
@@ -75,27 +75,27 @@
         case when {{first_date}} < {{second_date}}
             then ceil((
                 {# make sure the timestamps are real, otherwise raise an error asap #}
-                {{ spark_utils.assert_not_null('to_unix_timestamp', spark_utils.assert_not_null('to_timestamp', second_date)) }}
-                - {{ spark_utils.assert_not_null('to_unix_timestamp', spark_utils.assert_not_null('to_timestamp', first_date)) }}
+                {{ assert_not_null('to_unix_timestamp', assert_not_null('to_timestamp', second_date)) }}
+                - {{ assert_not_null('to_unix_timestamp', assert_not_null('to_timestamp', first_date)) }}
             ) / {{divisor}})
             else floor((
-                {{ spark_utils.assert_not_null('to_unix_timestamp', spark_utils.assert_not_null('to_timestamp', second_date)) }}
-                - {{ spark_utils.assert_not_null('to_unix_timestamp', spark_utils.assert_not_null('to_timestamp', first_date)) }}
+                {{ assert_not_null('to_unix_timestamp', assert_not_null('to_timestamp', second_date)) }}
+                - {{ assert_not_null('to_unix_timestamp', assert_not_null('to_timestamp', first_date)) }}
             ) / {{divisor}})
             end
-            
+
             {% if datepart == 'millisecond' %}
                 + cast(date_format({{second_date}}, 'SSS') as int)
                 - cast(date_format({{first_date}}, 'SSS') as int)
             {% endif %}
-            
-            {% if datepart == 'microsecond' %}    
+
+            {% if datepart == 'microsecond' %}
                 {% set capture_str = '[0-9]{4}-[0-9]{2}-[0-9]{2}.[0-9]{2}:[0-9]{2}:[0-9]{2}.([0-9]{6})' %}
                 -- Spark doesn't really support microseconds, so this is a massive hack!
                 -- It will only work if the timestamp-string is of the format
                 -- 'yyyy-MM-dd-HH mm.ss.SSSSSS'
                 + cast(regexp_extract({{second_date}}, '{{capture_str}}', 1) as int)
-                - cast(regexp_extract({{first_date}}, '{{capture_str}}', 1) as int)    
+                - cast(regexp_extract({{first_date}}, '{{capture_str}}', 1) as int)
             {% endif %}
 
     {%- else -%}
