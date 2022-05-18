cedffe195fd958135d72368607c1fb22e5bd2678
Jeremy Cohen <jeremy@dbtlabs.com>
2022-05-18 12:20:30 +0200

    Initialize lift + shift, dateadd + datediff

---
+++ tests/functional/adapter/test_utils.py
@@ -0,0 +1,10 @@
+from dbt.tests.adapter.utils.test_dateadd import BaseDateAdd
+from dbt.tests.adapter.utils.test_datediff import BaseDateDiff
+
+
+class TestDateAdd(BaseDateAdd):
+    pass
+
+
+class TestDateDiff(BaseDateDiff):
+    pass
