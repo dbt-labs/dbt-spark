b0cfcd3462f74d08fc73f340e884f3457bcddd3b
Jeremy Cohen <jeremy@dbtlabs.com>
2022-06-16 11:48:21 +0200

    Reorgnanize, skip, max for bool_or

--- tests/functional/adapter/utils/test_utils.py
+++ tests/functional/adapter/utils/test_utils.py
@@ -1,4 +1,5 @@
 import pytest
+
 from dbt.tests.adapter.utils.test_any_value import BaseAnyValue
 from dbt.tests.adapter.utils.test_bool_or import BaseBoolOr
 from dbt.tests.adapter.utils.test_cast_bool_to_text import BaseCastBoolToText
@@ -13,14 +14,17 @@ from dbt.tests.adapter.utils.test_hash import BaseHash
 from dbt.tests.adapter.utils.test_intersect import BaseIntersect
 from dbt.tests.adapter.utils.test_last_day import BaseLastDay
 from dbt.tests.adapter.utils.test_length import BaseLength
-from dbt.tests.adapter.utils.test_listagg import BaseListagg
-from dbt.tests.adapter.utils.fixture_listagg import models__test_listagg_yml
 from dbt.tests.adapter.utils.test_position import BasePosition
 from dbt.tests.adapter.utils.test_replace import BaseReplace
 from dbt.tests.adapter.utils.test_right import BaseRight
 from dbt.tests.adapter.utils.test_safe_cast import BaseSafeCast
 from dbt.tests.adapter.utils.test_split_part import BaseSplitPart
 from dbt.tests.adapter.utils.test_string_literal import BaseStringLiteral
+
+# requires modification
+from dbt.tests.adapter.utils.test_listagg import BaseListagg
+from dbt.tests.adapter.utils.fixture_listagg import models__test_listagg_yml
+from tests.functional.adapter.utils.fixture_listagg import models__test_listagg_no_order_by_sql
 
 
 class TestAnyValue(BaseAnyValue):
@@ -35,6 +39,7 @@ class TestCastBoolToText(BaseCastBoolToText):
     pass
 
 
+@pytest.mark.skip_profile('spark_session')
 class TestConcat(BaseConcat):
     pass
 
@@ -43,7 +48,7 @@ class TestDateAdd(BaseDateAdd):
     pass
 
 
-@pytest.mark.skip_profile('session')
+@pytest.mark.skip_profile('spark_session')
 class TestDateDiff(BaseDateDiff):
     pass
 
@@ -60,6 +65,7 @@ class TestExcept(BaseExcept):
     pass
 
 
+@pytest.mark.skip_profile('spark_session')
 class TestHash(BaseHash):
     pass
 
@@ -78,72 +84,13 @@ class TestLength(BaseLength):
 
 # SparkSQL does not support 'order by' for its 'listagg' equivalent
 # the argument is ignored, so let's ignore those fields when checking equivalency
-models__test_listagg_sql = """
-with data as (
-    select * from {{ ref('data_listagg') }}
-),
-data_output as (
-    select * from {{ ref('data_listagg_output') }}
-),
-calculate as (
-/*
-
-    select
-        group_col,
-        {{ listagg('string_text', "'_|_'", "order by order_col") }} as actual,
-        'bottom_ordered' as version
-    from data
-    group by group_col
-    union all
-    select
-        group_col,
-        {{ listagg('string_text', "'_|_'", "order by order_col", 2) }} as actual,
-        'bottom_ordered_limited' as version
-    from data
-    group by group_col
-    union all
-
-*/
-    select
-        group_col,
-        {{ listagg('string_text', "', '") }} as actual,
-        'comma_whitespace_unordered' as version
-    from data
-    where group_col = 3
-    group by group_col
-    union all
-    select
-        group_col,
-        {{ listagg('DISTINCT string_text', "','") }} as actual,
-        'distinct_comma' as version
-    from data
-    where group_col = 3
-    group by group_col
-    union all
-    select
-        group_col,
-        {{ listagg('string_text') }} as actual,
-        'no_params' as version
-    from data
-    where group_col = 3
-    group by group_col
-)
-select
-    calculate.actual,
-    data_output.expected
-from calculate
-left join data_output
-on calculate.group_col = data_output.group_col
-and calculate.version = data_output.version
-"""
-
 class TestListagg(BaseListagg):
     @pytest.fixture(scope="class")
     def models(self):
         return {
             "test_listagg.yml": models__test_listagg_yml,
             "test_listagg.sql": self.interpolate_macro_namespace(
-                models__test_listagg_sql, "listagg"
+                models__test_listagg_no_order_by_sql, "listagg"
             ),
         }
 
@@ -152,10 +99,12 @@ class TestPosition(BasePosition):
     pass
 
 
+@pytest.mark.skip_profile('spark_session')
 class TestReplace(BaseReplace):
     pass
 
 
+@pytest.mark.skip_profile('spark_session')
 class TestRight(BaseRight):
     pass
 
