66b8151e4d3ec320a1ab7d59942908872d02ae6a
Jeremy Cohen <jeremy@dbtlabs.com>
2022-07-11 12:04:52 +0200

    Account for refactor in dbt-labs/dbt-core@c763601

--- dbt/include/spark/macros/apply_grants.sql
+++ dbt/include/spark/macros/apply_grants.sql
@@ -14,39 +14,26 @@
     {% endif %}
 {% endmacro %}
 
-{%- macro spark__get_grant_sql(relation, grant_config) -%}
-    {%- set grant_statements = [] -%}
-    {%- for privilege in grant_config.keys() %}
-        {%- set grantees = grant_config[privilege] -%}
-        {%- for grantee in grantees -%}
-            {% set grant_sql -%}
-                grant {{ privilege }} on {{ relation }} to {{ adapter.quote(grantee) }}
-            {%- endset %}
-            {%- do grant_statements.append(grant_sql) -%}
-        {% endfor -%}
-    {%- endfor -%}
-    {{ return(grant_statements) }}
-{%- endmacro %}
 
-{%- macro spark__get_revoke_sql(relation, grant_config) -%}
-    {%- set revoke_statements = [] -%}
-    {%- for privilege in grant_config.keys() %}
-        {%- set grantees = grant_config[privilege] -%}
-        {%- for grantee in grantees -%}
-            {% set grant_sql -%}
-                revoke {{ privilege }} on {{ relation }} from {{ adapter.quote(grantee) }}
-            {%- endset %}
-            {%- do revoke_statements.append(grant_sql) -%}
-        {% endfor -%}
-    {%- endfor -%}
-    {{ return(revoke_statements) }}
+{%- macro spark__get_grant_sql(relation, privilege, grantees) -%}
+    grant {{ privilege }} on {{ relation }} to {{ adapter.quote(grantees[0]) }}
 {%- endmacro %}
 
 
-{% macro spark__call_grant_revoke_statement_list(grant_and_revoke_statement_list) %}
-    {% for grant_or_revoke_statement in grant_and_revoke_statement_list %}
+{%- macro spark__get_revoke_sql(relation, privilege, grantees) -%}
+    revoke {{ privilege }} on {{ relation }} from {{ adapter.quote(grantees[0]) }}
+{%- endmacro %}
+
+
+{%- macro spark__support_multiple_grantees_per_dcl_statement() -%}
+    {{ return(False) }}
+{%- endmacro -%}
+
+
+{% macro spark__call_dcl_statements(dcl_statement_list) %}
+    {% for dcl_statement in dcl_statement_list %}
         {% call statement('grant_or_revoke') %}
-            {{ grant_or_revoke_statement }}
+            {{ dcl_statement }}
         {% endcall %}
     {% endfor %}
 {% endmacro %}
