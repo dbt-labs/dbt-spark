d4495c6a6bcde8ea4bda6cb4a6a407f5977b936e
V. Ganesh <tovganesh@yahoo.co.in>
2022-07-29 21:56:53 +0530

    update version number to prepare for pypi release (#15)

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def _get_dbt_core_version():
 
 
 package_name = "dbt-spark-livy"
-package_version = "1.1.2"
+package_version = "1.1.3"
 dbt_core_version = _get_dbt_core_version()
 description = """The dbt-spark-livy adapter plugin for Spark in Cloudera DataHub with Livy interface"""
 
