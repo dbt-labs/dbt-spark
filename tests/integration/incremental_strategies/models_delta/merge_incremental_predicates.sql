1de64d1f782cb9680a39d3de8b1554f64d8c8ad3
Dave Connors <dave.connors@fishtownanalytics.com>
2022-08-22 17:01:42 -0500

    test merge predicates

---
+++ tests/integration/incremental_strategies/models_delta/merge_incremental_predicates.sql
@@ -0,0 +1,24 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    file_format = 'delta',
+    unique_key = 'id',
+    incremental_predicates = [
+        "dbt_internal_dest.id != 2"
+    ],
+) }}
+
+{% if not is_incremental() %}
+
+select cast(1 as bigint) as id, 'hello' as msg, 'blue' as color
+union all
+select cast(2 as bigint) as id, 'goodbye' as msg, 'red' as color
+
+{% else %}
+
+-- merge will not happen on the above record where id = 2, so new record will fall to insert
+select cast(2 as bigint) as id, 'yo' as msg, 'green' as color
+union all
+select cast(3 as bigint) as id, 'anyway' as msg, 'purple' as color
+
+{% endif %}
