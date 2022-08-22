32712b08f7c4c6791055cd19c4440613a7c067ef
Dave Connors <dave.connors@fishtownanalytics.com>
2022-08-22 17:09:08 -0500

    add update row

--- tests/integration/incremental_strategies/models_delta/merge_incremental_predicates.sql
+++ tests/integration/incremental_strategies/models_delta/merge_incremental_predicates.sql
@@ -17,6 +17,8 @@ select cast(2 as bigint) as id, 'goodbye' as msg, 'red' as color
 {% else %}
 
 -- merge will not happen on the above record where id = 2, so new record will fall to insert
+select cast(1 as bigint) as id, 'hey' as msg, 'blue' as color
+union all
 select cast(2 as bigint) as id, 'yo' as msg, 'green' as color
 union all
 select cast(3 as bigint) as id, 'anyway' as msg, 'purple' as color
