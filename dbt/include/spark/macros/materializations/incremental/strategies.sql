ebd011ea71ba533e065cb167f6c8213753fa6e9e
Chenyu Li <chenyu.li@dbtlabs.com>
2022-08-31 07:21:14 -0700

    set tmp relation with proper schema (#445)
    * set tmp relation with proper schema
    
    * add changelog

--- dbt/include/spark/macros/materializations/incremental/strategies.sql
+++ dbt/include/spark/macros/materializations/incremental/strategies.sql
@@ -4,7 +4,7 @@
     {%- set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') -%}
     insert overwrite table {{ target_relation }}
     {{ partition_cols(label="partition") }}
-    select {{dest_cols_csv}} from {{ source_relation.include(database=false, schema=false) }}
+    select {{dest_cols_csv}} from {{ source_relation }}
 
 {% endmacro %}
 
@@ -14,7 +14,7 @@
     {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
     {%- set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') -%}
     insert into table {{ target_relation }}
-    select {{dest_cols_csv}} from {{ source_relation.include(database=false, schema=false) }}
+    select {{dest_cols_csv}} from {{ source_relation }}
 
 {% endmacro %}
 
@@ -45,7 +45,7 @@
   {{ sql_header if sql_header is not none }}
 
   merge into {{ target }} as DBT_INTERNAL_DEST
-      using {{ source.include(schema=false) }} as DBT_INTERNAL_SOURCE
+      using {{ source }} as DBT_INTERNAL_SOURCE
       on {{ predicates | join(' and ') }}
 
       when matched then update set
