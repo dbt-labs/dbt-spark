82aacf9ee21a1c72900d40ff6c241066c4de05c4
Colin <colin.rogers@dbtlabs.com>
2022-09-27 11:10:47 -0700

    add backcompat to test fixture

--- tests/functional/adapter/utils/test_timestamps.py
+++ tests/functional/adapter/utils/test_timestamps.py
@@ -5,4 +5,8 @@ import pytest
 class TestCurrentTimestampSnowflake(test_timestamps.TestCurrentTimestamps):
     @pytest.fixture(scope="class")
     def expected_schema(self):
-        return {"current_timestamp": "timestamp", "current_timestamp_in_utc": "timestamp"}
+        return {
+            "current_timestamp": "timestamp",
+            "current_timestamp_in_utc": "timestamp",
+            "current_timestamp_backcompat": "timestamp",
+        }
