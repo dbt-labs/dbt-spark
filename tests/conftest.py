e199bd6d855d8d62d77817f45a1c4faef80b7805
Chenyu Li <chenyu.li@dbtlabs.com>
2022-09-28 14:11:57 -0700

    change to run with mark

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import pytest
 import os
-from xdist import is_xdist_controller
-from xdist.scheduler.loadscope import LoadScopeScheduling
+
 
 pytest_plugins = ["dbt.tests.fixtures.project"]
 
@@ -13,7 +12,6 @@ def pytest_addoption(parser):
 # Using @pytest.mark.skip_profile('apache_spark') uses the 'skip_by_profile_type'
 # autouse fixture below
 def pytest_configure(config):
-    config.pluginmanager.register(XDistSerialPlugin())
     config.addinivalue_line(
         "markers",
         "skip_profile(profile): skip test for the given profile",
@@ -118,37 +116,3 @@ def skip_by_profile_type(request):
         for skip_profile_type in request.node.get_closest_marker("skip_profile").args:
             if skip_profile_type == profile_type:
                 pytest.skip(f"skipped on '{profile_type}' profile")
-
-
-# solution from https://github.com/pytest-dev/pytest-xdist/issues/385#issuecomment-1176976154
-class XDistSerialPlugin:
-    def __init__(self):
-        self._nodes = None
-
-    @pytest.hookimpl(tryfirst=True)
-    def pytest_collection(self, session):
-        if is_xdist_controller(session):
-            self._nodes = {
-                item.nodeid: item
-                for item in session.perform_collect(None)
-            }
-            return True
-
-    def pytest_xdist_make_scheduler(self, config, log):
-        return SerialScheduling(config, log, nodes=self._nodes)
-
-
-class SerialScheduling(LoadScopeScheduling):
-    def __init__(self, config, log, *, nodes):
-        super().__init__(config, log)
-        self._nodes = nodes
-
-    def _split_scope(self, nodeid):
-        node = self._nodes[nodeid]
-        if node.get_closest_marker("serial"):
-            # put all `@pytest.mark.serial` tests in same scope, to
-            # ensure they're all run in the same worker
-            return "__serial__"
-
-        # otherwise, each test is in its own scope
-        return nodeid
