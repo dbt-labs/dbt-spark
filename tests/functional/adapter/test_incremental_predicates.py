1fab80155ed7b70e21f370443cf7de6fbb20ec4f
Dave Connors <dave.connors@fishtownanalytics.com>
2022-11-04 13:46:08 -0500

    add functional test

---
+++ tests/functional/adapter/test_incremental_predicates.py
@@ -0,0 +1,15 @@
+import pytest
+from dbt.tests.adapter.incremental.test_incremental_predicates import BaseIncrementalPredicates
+
+
+class TestIncrementalPredicatesMergeSpark(BaseIncrementalPredicates):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": { 
+                "+incremental_predicates": [
+                    "dbt_internal_dest.id != 2"
+                ],
+                "+incremental_strategy": "merge"
+            }
+        }
