12a90d0906ece9bd4c3413c06ed865197c7d4e09
Dave Connors <dave.connors@fishtownanalytics.com>
2022-11-04 14:25:10 -0500

    update SQL to spark syntax

--- tests/functional/adapter/test_incremental_predicates.py
+++ tests/functional/adapter/test_incremental_predicates.py
@@ -2,6 +2,31 @@ import pytest
 from dbt.tests.adapter.incremental.test_incremental_predicates import BaseIncrementalPredicates
 
 
+models__spark_incremental_predicates_sql = """
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'id'
+) }}
+
+{% if not is_incremental() %}
+
+select cast(1 as bigint) as id, 'hello' as msg, 'blue' as color
+union all
+select cast(2 as bigint) as id, 'goodbye' as msg, 'red' as color
+
+{% else %}
+
+-- merge will not happen on the above record where id = 2, so new record will fall to insert
+select cast(1 as bigint) as id, 'hey' as msg, 'blue' as color
+union all
+select cast(2 as bigint) as id, 'yo' as msg, 'green' as color
+union all
+select cast(3 as bigint) as id, 'anyway' as msg, 'purple' as color
+
+{% endif %}
+"""
+
+
 class TestIncrementalPredicatesMergeSpark(BaseIncrementalPredicates):
     @pytest.fixture(scope="class")
     def project_config_update(self):
@@ -13,4 +38,9 @@ class TestIncrementalPredicatesMergeSpark(BaseIncrementalPredicates):
                 "+incremental_strategy": "merge",
                 "+file_format": "delta"
             }
+        }
+    
+    def models(self):
+        return {
+            "delete_insert_incremental_predicates.sql": models__spark_incremental_predicates_sql
         }
