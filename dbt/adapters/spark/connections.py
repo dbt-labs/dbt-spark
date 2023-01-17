9b7bcde3d951bfa6a7cbf06e3536dc9b70f8075e
VShkaberda <v.shkaberda@gmail.com>
2023-01-17 17:03:53 +0200

    Minor change: line length and commas.

--- dbt/adapters/spark/connections.py
+++ dbt/adapters/spark/connections.py
@@ -378,7 +378,10 @@ class SparkConnectionManager(SQLConnectionManager):
                             kerberos_service_name=creds.kerberos_service_name,
                             password=creds.password,
                         )
-                        conn = hive.connect(thrift_transport=transport, configuration=creds.server_side_parameters)
+                        conn = hive.connect(
+                            thrift_transport=transport,
+                            configuration=creds.server_side_parameters,
+                        )
                     else:
                         conn = hive.connect(
                             host=creds.host,
@@ -387,7 +390,7 @@ class SparkConnectionManager(SQLConnectionManager):
                             auth=creds.auth,
                             kerberos_service_name=creds.kerberos_service_name,
                             password=creds.password,
-                            configuration=creds.server_side_parameters
+                            configuration=creds.server_side_parameters,
                         )  # noqa
                     handle = PyhiveConnectionWrapper(conn)
                 elif creds.method == SparkConnectionMethod.ODBC:
