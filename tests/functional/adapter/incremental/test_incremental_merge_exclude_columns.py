83ef03316f7b3fba78e87bb3a0d1f4c8c12b2f2f
Dave Connors <dave.connors@fishtownanalytics.com>
2023-01-23 14:03:13 -0600

    delta file format config

--- tests/functional/adapter/incremental/test_incremental_merge_exclude_columns.py
+++ tests/functional/adapter/incremental/test_incremental_merge_exclude_columns.py
@@ -4,5 +4,7 @@ from dbt.tests.util import run_dbt
 from dbt.tests.adapter.incremental.test_incremental_merge_exclude_columns import BaseMergeExcludeColumns
 
 class TestMergeExcludeColumns(BaseMergeExcludeColumns):
-    pass
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {"models": {"file_format": "delta"}}
 
