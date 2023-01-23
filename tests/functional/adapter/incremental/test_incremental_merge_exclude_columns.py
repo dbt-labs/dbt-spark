7632da2e0d1f036eb56e2f18f2beb870a74a5ccc
Dave Connors <dave.connors@fishtownanalytics.com>
2023-01-23 13:48:55 -0600

    add adapter test

---
+++ tests/functional/adapter/incremental/test_incremental_merge_exclude_columns.py
@@ -0,0 +1,8 @@
+import pytest
+
+from dbt.tests.util import run_dbt
+from dbt.tests.adapter.incremental.test_incremental_merge_exclude_columns import BaseMergeExcludeColumns
+
+class TestMergeExcludeColumns(BaseMergeExcludeColumns):
+    pass
+
