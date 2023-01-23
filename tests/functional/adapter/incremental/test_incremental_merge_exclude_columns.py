c65b65baadbb75214d57535f3a9763a6901e69df
Dave Connors <dave.connors@fishtownanalytics.com>
2023-01-23 14:19:50 -0600

    skip profile

--- tests/functional/adapter/incremental/test_incremental_merge_exclude_columns.py
+++ tests/functional/adapter/incremental/test_incremental_merge_exclude_columns.py
@@ -3,8 +3,6 @@ import pytest
 from dbt.tests.util import run_dbt
 from dbt.tests.adapter.incremental.test_incremental_merge_exclude_columns import BaseMergeExcludeColumns
 
+@pytest.mark.skip_profile('spark_session', 'apache_spark')
 class TestMergeExcludeColumns(BaseMergeExcludeColumns):
-    @pytest.fixture(scope="class")
-    def project_config_update(self):
-        return {"models": {"file_format": "delta"}}
-
+    pass
