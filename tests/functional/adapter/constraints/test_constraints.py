794570f75c1321e1b7c2f42a179859988ebd718d
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2023-02-01 11:01:52 +0100

    Use new testing framework

---
+++ tests/functional/adapter/constraints/test_constraints.py
@@ -0,0 +1,53 @@
+import pytest
+import re
+import json
+from dbt.tests.util import (
+    run_dbt,
+    get_manifest,
+    run_dbt_and_capture
+)
+from dbt.tests.adapter.constraints.test_constraints import (
+  BaseConstraintsColumnsEqual,
+  BaseConstraintsRuntimeEnforcement
+)
+
+_expected_sql_spark = """
+  create  table
+    "{0}"."{1}"."my_model__dbt_tmp"
+    
+  (
+      id integer not null,
+      color text,
+      date_day date,
+      primary key(id)
+  )
+    
+  
+    
+    
+    
+  ;
+  insert into "{0}"."{1}"."my_model__dbt_tmp"
+    (
+      
+select
+  1 as id,
+  'blue' as color,
+  cast('2019-01-01' as date) as date_day
+    )
+  ;
+"""
+
+@pytest.mark.skip_profile('spark_session', 'apache_spark')
+class TestSparkConstraintsColumnsEqual(BaseConstraintsColumnsEqual):
+    pass
+
+@pytest.mark.skip_profile('spark_session', 'apache_spark')
+class TestSparkConstraintsRuntimeEnforcement(BaseConstraintsRuntimeEnforcement):
+    @pytest.fixture(scope="class")
+    def expected_sql(self, project):
+        return _expected_sql_spark.format(project.database, project.test_schema)
+
+    @pytest.fixture(scope="class")
+    def expected_error_messages(self):
+        return ['violate the new NOT NULL constraint']
