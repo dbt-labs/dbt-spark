fd8f618b33feec3086b693e97e8ee44c16e20827
Colin <colin.rogers@dbtlabs.com>
2023-02-02 10:36:43 -0800

    convert test_store_test_failures to functional

--- tests/functional/adapter/store_test_failures_tests/test_store_test_failures.py
+++ tests/functional/adapter/store_test_failures_tests/test_store_test_failures.py
@@ -2,14 +2,14 @@ import pytest
 
 from dbt.tests.adapter.store_test_failures_tests.test_store_test_failures import StoreTestFailuresBase
 
-
+@pytest.mark.skip_profile('spark_session')
 class TestSparkStoreTestFailures(StoreTestFailuresBase):
 
     def test_store_and_assert(self, project):
         self.run_tests_store_one_failure(project)
         self.run_tests_store_failures_and_assert(project)
 
-
+@pytest.mark.skip_profile('apache_spark', 'spark_session')
 class TestSparkStoreTestFailuresWithDelta(StoreTestFailuresBase):
     @pytest.fixture(scope="class")
     def project_config_update(self):
