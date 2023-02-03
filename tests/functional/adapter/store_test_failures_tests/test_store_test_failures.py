80a0574d59d9fa6013e292ff7391d3de9b512b3f
Colin <colin.rogers@dbtlabs.com>
2023-02-02 17:59:23 -0800

    specify test schema

--- tests/functional/adapter/store_test_failures_tests/test_store_test_failures.py
+++ tests/functional/adapter/store_test_failures_tests/test_store_test_failures.py
@@ -1,12 +1,16 @@
 import pytest
 
-from dbt.tests.adapter.store_test_failures_tests.test_store_test_failures import StoreTestFailuresBase
+from dbt.tests.adapter.store_test_failures_tests.test_store_test_failures import StoreTestFailuresBase, \
+    TEST_AUDIT_SCHEMA_SUFFIX
+
 
 @pytest.mark.skip_profile('spark_session')
 class TestSparkStoreTestFailures(StoreTestFailuresBase):
+
     def test_store_and_assert(self, project):
         self.run_tests_store_one_failure(project)
         self.run_tests_store_failures_and_assert(project)
+
 
 @pytest.mark.skip_profile('apache_spark', 'spark_session')
 class TestSparkStoreTestFailuresWithDelta(StoreTestFailuresBase):
@@ -14,8 +18,8 @@ class TestSparkStoreTestFailuresWithDelta(StoreTestFailuresBase):
     def project_config_update(self):
         return {
             'tests': {
+                "+schema": TEST_AUDIT_SCHEMA_SUFFIX,
                 '+store_failures': True,
-                '+severity': 'warn',
                 '+file_format': 'delta',
             }
         }
@@ -23,3 +27,4 @@ class TestSparkStoreTestFailuresWithDelta(StoreTestFailuresBase):
     def test_store_and_assert_failure_with_delta(self, project):
         self.run_tests_store_one_failure(project)
         self.run_tests_store_failures_and_assert(project)
+
