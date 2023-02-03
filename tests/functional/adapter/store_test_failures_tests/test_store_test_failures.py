336c6ac82ccf0019a2d7699ee837a26df0260a6d
Colin <colin.rogers@dbtlabs.com>
2023-02-03 11:00:58 -0800

    test specify delta in seeds schema

--- tests/functional/adapter/store_test_failures_tests/test_store_test_failures.py
+++ tests/functional/adapter/store_test_failures_tests/test_store_test_failures.py
@@ -14,9 +14,15 @@ class TestSparkStoreTestFailures(StoreTestFailuresBase):
 
 @pytest.mark.skip_profile('apache_spark', 'spark_session')
 class TestSparkStoreTestFailuresWithDelta(StoreTestFailuresBase):
+
     @pytest.fixture(scope="class")
     def project_config_update(self):
         return {
+            "seeds": {
+                "quote_columns": False,
+                "test": self.column_type_overrides(),
+                "+file_format": "delta",
+            },
             'tests': {
                 "+schema": TEST_AUDIT_SCHEMA_SUFFIX,
                 '+store_failures': True,
@@ -27,4 +33,3 @@ class TestSparkStoreTestFailuresWithDelta(StoreTestFailuresBase):
     def test_store_and_assert_failure_with_delta(self, project):
         self.run_tests_store_one_failure(project)
         self.run_tests_store_failures_and_assert(project)
-
