cc7c7b229423ad65bb07ed10dba837a518a31e04
Michelle Ark <michelle.ark@dbtlabs.com>
2023-03-03 19:31:22 -0500

    test array type on TestSparkConstraintsColumnsEqualPyodbc

--- tests/functional/adapter/test_constraints.py
+++ tests/functional/adapter/test_constraints.py
@@ -54,9 +54,13 @@ class TestSparkConstraintsColumnsEqualPyodbc(BaseConstraintsColumnsEqual):
     def data_types(self, int_type, schema_int_type, string_type):
         # sql_column_value, schema_data_type, error_data_type
         return [
+            # TODO: the int type is tricky to test in test__constraints_wrong_column_order without a schema_string_type to override.
+            # uncomment the line below once https://github.com/dbt-labs/dbt-core/issues/7121 is resolved
             # ['1', schema_int_type, int_type],
             ['"1"', "string", string_type],
             ["true", "boolean", "BOOL"],
+            ['array("1","2","3")', "array<string>", "LIST"],
+            ['array(1,2,3)', "array<int>", "LIST"],
         ]
 
 
@@ -86,6 +90,8 @@ class TestSparkConstraintsColumnsEqualDatabricksHTTP(BaseConstraintsColumnsEqual
     def data_types(self, int_type, schema_int_type, string_type):
         # sql_column_value, schema_data_type, error_data_type
         return [
+            # TODO: the int type is tricky to test in test__constraints_wrong_column_order without a schema_string_type to override.
+            # uncomment the line below once https://github.com/dbt-labs/dbt-core/issues/7121 is resolved
             # ['1', schema_int_type, int_type],
             ['"1"', "string", string_type],
             ["true", "boolean", "BOOLEAN_TYPE"],
