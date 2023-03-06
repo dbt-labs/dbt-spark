736fbfbc4f9c1a8948f22d67e184a36e04bbc813
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-03-06 15:21:35 -0600

    init push of adding two new bash scripts to be triggered by cut-release-branch workflow to automate release process

---
+++ .github/scripts/update_release_branch.sh
@@ -0,0 +1,12 @@
+# TODO uncomment once dbt-spark is off CircleCI
+#!/bin/bash -e
+# set -e
+
+# release_branch=$1
+# target_req_file=".github/workflows/nightly-release.yml"
+# if [[ "$OSTYPE" == darwin* ]]; then
+#  # mac ships with a different version of sed that requires a delimiter arg
+#  sed -i "" "s|[0-9].[0-9].latest|$release_branch|" $target_req_file
+# else
+#  sed -i "s|[0-9].[0-9].latest|$release_branch|" $target_req_file
+# fi
