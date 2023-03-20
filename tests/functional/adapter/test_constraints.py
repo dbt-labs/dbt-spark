fe999abd42681330d16f710e0b67373db990b87f
Emily Rockman <emily.rockman@dbtlabs.com>
2023-03-20 15:28:50 -0500

    add another error msg

--- tests/functional/adapter/test_constraints.py
+++ tests/functional/adapter/test_constraints.py
@@ -209,8 +209,9 @@ class BaseSparkConstraintsRollbackSetup:
         return [
             "violate the new CHECK constraint",
             "DELTA_NEW_CHECK_CONSTRAINT_VIOLATION",
-            "DELTA_VIOLATE_CONSTRAINT_WITH_VALUES",
             "violate the new NOT NULL constraint",
+            "(id > 0) violated by row with values:",  # incremental mats
+            "DELTA_VIOLATE_CONSTRAINT_WITH_VALUES",  # incremental mats
         ]
 
     def assert_expected_error_messages(self, error_message, expected_error_messages):
