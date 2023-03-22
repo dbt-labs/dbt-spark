63cd8724156a72ed6b6976a6dc2f419726997b82
Emily Rockman <emily.rockman@dbtlabs.com>
2023-03-22 12:50:27 -0500

    persist constraints for incremental mats

--- dbt/include/spark/macros/materializations/incremental/incremental.sql
+++ dbt/include/spark/macros/materializations/incremental/incremental.sql
@@ -76,6 +76,8 @@
 
   {% do persist_docs(target_relation, model) %}
 
+  {% do persist_constraints(target_relation, model) %}
+
   {{ run_hooks(post_hooks) }}
 
   {{ return({'relations': [target_relation]}) }}
