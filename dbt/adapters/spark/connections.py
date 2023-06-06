6b9c763b12e74a62761bd12e90973bec3adda23e
Mila Page <versusfacit@users.noreply.github.com>
2023-06-06 11:34:05 -0700

    Fix tests caused by core merge of equal macro changes.

--- dbt/adapters/spark/connections.py
+++ dbt/adapters/spark/connections.py
@@ -149,27 +149,7 @@ class SparkCredentials(Credentials):
         return self.host
 
     def _connection_keys(self) -> Tuple[str, ...]:
-        return (
-            "host",
-            "port",
-            "cluster",
-            "endpoint",
-            "schema",
-            "organization",
-            "method",
-            "database",
-            "driver",
-            "token",
-            "user",
-            "password",
-            "auth",
-            "kerberos_service_name",
-            "connect_retries",
-            "connect_timeout",
-            "use_ssl",
-            "server_side_parameters",
-            "retry_all",
-        )
+        return ("host", "port", "cluster", "endpoint", "schema", "organization")
 
 
 class PyhiveConnectionWrapper(object):
