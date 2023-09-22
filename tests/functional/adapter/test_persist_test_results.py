b9f920910bfd80a33d1893e2e87ea235ccd41252
Mike Alfare <mike.alfare@dbtlabs.com>
2023-09-21 23:28:04 -0400

    add Spark config

--- tests/functional/adapter/test_persist_test_results.py
+++ tests/functional/adapter/test_persist_test_results.py
@@ -3,6 +3,16 @@ import pytest
 from dbt.tests.adapter.persist_test_results.basic import PersistTestResults
 
 
-@pytest.mark.skip_profile("apache_spark", "spark_session")
-class TestPersistTestResults(PersistTestResults):
+@pytest.mark.skip_profile("spark_session", "apache_spark")
+class TestPersistTestResultsDatabricks(PersistTestResults):
     pass
+
+
+@pytest.mark.skip_profile("spark_session", "databricks_cluster", "databricks_sql_endpoint")
+class TestPersistTestResultsSpark(PersistTestResults):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "seeds": {"quote_columns": True},
+            "tests": {"+schema": self.audit_schema_suffix},
+        }
