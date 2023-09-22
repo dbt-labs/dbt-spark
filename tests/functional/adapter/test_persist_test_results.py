1ea46bcd7ae096db32dd4467aa8fabe09e8309ba
Mike Alfare <mike.alfare@dbtlabs.com>
2023-09-21 22:21:03 -0400

    skip Spark, which needs different delete sql

--- tests/functional/adapter/test_persist_test_results.py
+++ tests/functional/adapter/test_persist_test_results.py
@@ -1,5 +1,8 @@
+import pytest
+
 from dbt.tests.adapter.persist_test_results.basic import PersistTestResults
 
 
+@pytest.mark.skip_profile("apache_spark", "spark_session")
 class TestPersistTestResults(PersistTestResults):
     pass
