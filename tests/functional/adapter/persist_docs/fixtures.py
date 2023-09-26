cbb3a375debab9df229041e7d47987b26849593c
colin-rogers-dbt <111200756+colin-rogers-dbt@users.noreply.github.com>
2023-09-26 13:12:27 -0700

    persist view column comments (#893)
    * persist view column comments
    
    * format: whitespace
    
    * extracted get_matched_column macro
    
    * move parenthesis to the calling macro
    
    * changelog
    
    * fix: remove matching column in different case
    
    * fix: remove get_matched_column  macro - not much logic left there.
    
    * escape column comments and add functional test
    
    * Update Features-20230817-130731.yaml
    
    * remove unneeded f string
    
    * add test fixture to view test
    
    * fix fixtures for TestPersistDocsDeltaView
    
    * fix fixtures for TestPersistDocsDeltaView
    
    * formatting
    
    * fix tests
    
    ---------
    
    Co-authored-by: Juri Krainjukov <juri.krainjukov@gmail.com>
    (cherry picked from commit 1672efd4fff26f884decec22b8f02924e7a91976)

--- tests/functional/adapter/persist_docs/fixtures.py
+++ tests/functional/adapter/persist_docs/fixtures.py
@@ -21,11 +21,39 @@ _MODELS__TABLE_DELTA_MODEL = """
 select 1 as id, 'Joe' as name
 """
 
+_MODELS__VIEW_DELTA_MODEL = """
+{{ config(materialized='view') }}
+select id, count(*) as count from {{ ref('table_delta_model') }} group by id
+"""
+
 _MODELS__TABLE_DELTA_MODEL_MISSING_COLUMN = """
 {{ config(materialized='table', file_format='delta') }}
 select 1 as id, 'Joe' as different_name
 """
+_VIEW_PROPERTIES_MODELS = """
+version: 2
 
+models:
+  - name: view_delta_model
+    description: |
+      View model description "with double quotes"
+      and with 'single  quotes' as welll as other;
+      '''abc123'''
+      reserved -- characters
+      --
+      /* comment */
+      Some $lbl$ labeled $lbl$ and $$ unlabeled $$ dollar-quoting
+    columns:
+      - name: id
+        description: |
+          id Column description "with double quotes"
+          and with 'single  quotes' as welll as other;
+          '''abc123'''
+          reserved -- characters
+          --
+          /* comment */
+          Some $lbl$ labeled $lbl$ and $$ unlabeled $$ dollar-quoting
+"""
 _PROPERTIES__MODELS = """
 version: 2
 
