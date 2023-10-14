06680dac77ca4a86b4b555439c2ee7abd61159f4
Manrique Vargas <mv1742@nyu.edu>
2023-10-13 19:18:02 -0600

    add truncate table function

--- dbt/include/spark/macros/adapters.sql
+++ dbt/include/spark/macros/adapters.sql
@@ -342,6 +342,13 @@
   {%- endcall %}
 {% endmacro %}
 
+
+{% macro spark__truncate_relation(relation) -%}
+  {% call statement('truncate_relation', auto_begin=False) -%}
+    truncate {{ relation.type }} if exists {{ relation }}
+  {%- endcall %}
+{% endmacro %}
+
 {% macro spark__drop_relation(relation) -%}
   {% call statement('drop_relation', auto_begin=False) -%}
     drop {{ relation.type }} if exists {{ relation }}
