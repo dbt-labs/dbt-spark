bf6e9108dedf0ff920b06075c68102b2acfcaf77
Mike Alfare <mike.alfare@dbtlabs.com>
2023-12-16 12:23:39 -0500

    manually create schema_map so that it's limited to the relations provided

--- dbt/adapters/spark/impl.py
+++ dbt/adapters/spark/impl.py
@@ -3,12 +3,11 @@ from concurrent.futures import Future
 from dataclasses import dataclass
 from typing import Any, Dict, Iterable, List, Optional, Union, Type, Tuple, Callable, Set
 
-from dbt.adapters.base.relation import InformationSchema
+from dbt.adapters.base.relation import InformationSchema, SchemaSearchMap
 from dbt.adapters.capability import CapabilityDict, CapabilitySupport, Support, Capability
 from dbt.contracts.graph.manifest import Manifest
 
 from typing_extensions import TypeAlias
-
 import agate
 
 import dbt
@@ -385,7 +384,26 @@ class SparkAdapter(SQLAdapter):
     def get_catalog_by_relations(
         self, manifest: Manifest, relations: Set[BaseRelation]
     ) -> Tuple[agate.Table, List[Exception]]:
-        return self.get_catalog(manifest)
+        schema_map = SchemaSearchMap()
+        for relation in relations:
+            schema_map.add(relation)
+
+        with executor(self.config) as tpe:
+            futures: List[Future[agate.Table]] = []
+            for info, schemas in schema_map.items():
+                for schema in schemas:
+                    futures.append(
+                        tpe.submit_connected(
+                            self,
+                            schema,
+                            self._get_one_catalog_by_relations,
+                            info,
+                            [schema],
+                            manifest,
+                        )
+                    )
+            catalogs, exceptions = catch_as_completed(futures)
+        return catalogs, exceptions
 
     def _get_one_catalog(
         self,
