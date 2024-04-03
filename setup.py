e398da11ed01837a9704b20ae0038a2a4556efc0
Mike Alfare <mike.alfare@dbtlabs.com>
2024-04-03 13:52:34 -0400

    add `dbt-core~=1.8.0a1` as convenience dep

--- setup.py
+++ setup.py
@@ -67,6 +67,8 @@ setup(
         "sqlparams>=3.0.0",
         "dbt-common>=0.1.0a1,<2.0",
         "dbt-adapters>=0.1.0a1,<2.0",
+        # add dbt-core to ensure backwards compatibility of installation, this is not a functional dependency
+        "dbt-core>=1.8.0a1",
     ],
     extras_require={
         "ODBC": odbc_extras,
