c1f735907856aea0f67d982050bebf1e31cd4f72
Mike Alfare <mike.alfare@dbtlabs.com>
2024-05-02 12:24:29 -0400

    clean up test script

--- docker-release/test.sh
+++ docker-release/test.sh
@@ -1,22 +1,19 @@
 # - VERY rudimentary test script to run latest + specific branch image builds and test them all by running `--version`
 # TODO: create a real test suite
+set -e
 
-clear \
-&& echo "\n\n"\
-"########################################\n"\
-"##### Testing dbt-spark latest #####\n"\
-"########################################\n"\
-&& docker build --tag dbt-spark \
-  --target dbt-spark \
-  docker \
-&& docker run dbt-spark --version \
-\
-&& echo "\n\n"\
-"#########################################\n"\
-"##### Testing dbt-spark-1.0.0b1 #####\n"\
-"#########################################\n"\
-&& docker build --tag dbt-spark-1.0.0b1 \
-  --target dbt-spark \
-  --build-arg commit_ref=v1.0.0b1 \
-  docker \
-&& docker run dbt-spark-1.0.0b1 --version
+echo "\n\n"
+echo "####################################"
+echo "##### Testing dbt-spark latest #####"
+echo "####################################"
+
+docker build --tag dbt-spark --target dbt-spark docker
+docker run dbt-spark --version
+
+echo "\n\n"
+echo "#####################################"
+echo "##### Testing dbt-spark-1.0.0b1 #####"
+echo "#####################################"
+
+docker build --tag dbt-spark-1.0.0b1 --target dbt-spark --build-arg commit_ref=v1.0.0b1 docker
+docker run dbt-spark-1.0.0b1 --version
