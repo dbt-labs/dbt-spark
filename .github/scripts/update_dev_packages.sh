3a3a3afc6bc57961f5c9ab82d3a4ed03621d708b
Gerda Shank <gerda@dbtlabs.com>
2024-07-24 10:52:34 -0400

    Update update_dev_packages.sh

--- .github/scripts/update_dev_packages.sh
+++ .github/scripts/update_dev_packages.sh
@@ -6,7 +6,7 @@ adapters_git_branch=$1
 core_git_branch=$2
 target_req_file="dev-requirements.txt"
 core_req_sed_pattern="s|dbt-core.git.*#egg=dbt-core|dbt-core.git@${core_git_branch}#egg=dbt-core|g"
-adapters_req_sed_pattern="s|dbt-adapters.git.*#egg=dbt-adapters|dbt-adapters.git@${adapters_git_branch}#egg=dbt-adapters|g"
+adapters_req_sed_pattern="s|dbt-adapters.git|dbt-adapters.git@${adapters_git_branch}|g"
 if [[ "$OSTYPE" == darwin* ]]; then
  # mac ships with a different version of sed that requires a delimiter arg
  sed -i "" "$core_req_sed_pattern" $target_req_file
