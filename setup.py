8dc6b6ee5c06cb7221ba53fbf3499331db20a2e6
Mila Page <versusfacit@users.noreply.github.com>
2024-07-28 00:18:27 -0700

    Add support for 3.12 in workflow files and configs.

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     python_requires=">=3.8",
 )
