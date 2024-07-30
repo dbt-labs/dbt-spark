03a0b7685c48b156664c2311db50aa32c027564d
McKnight-42 <matthew.mcknight@dbtlabs.com>
2024-07-30 15:16:25 -0500

    slight change to possibly unneeded -openbsd callout

--- dagger/spark-container/install_spark.sh
+++ dagger/spark-container/install_spark.sh
@@ -4,7 +4,7 @@ SPARK_VERSION=3.1.3
 HADOOP_VERSION=3.2
 
 apt-get update && \
-apt-get install -y wget netcat -openbsd procps libpostgresql-jdbc-java && \
+apt-get install -y wget netcat procps libpostgresql-jdbc-java && \
 wget -q "https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz" && \
 tar xzf "spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz" && \
 rm "spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz" && \
