55015490aeea3e924ae2e924c2dd25f1eeaad2c5
Michelle Ark <michelle.ark@dbtlabs.com>
2024-09-24 21:37:07 +0100

    add begin config + create date_day

--- tests/functional/adapter/incremental_strategies/test_microbatch.py
+++ tests/functional/adapter/incremental_strategies/test_microbatch.py
@@ -6,8 +6,9 @@ from dbt.tests.adapter.incremental.test_incremental_microbatch import (
 
 # No requirement for a unique_id for spark microbatch!
 _microbatch_model_no_unique_id_sql = """
-{{ config(materialized='incremental', incremental_strategy='microbatch', event_time='event_time', batch_size='day') }}
-select * from {{ ref('input_model') }}
+{{ config(materialized='incremental', incremental_strategy='microbatch', event_time='event_time', batch_size='day', begin=modules.datetime.datetime(2020, 1, 1, 0, 0, 0)) }}
+select *, cast(event_time as date) as date_day
+from {{ ref('input_model') }}
 """
 
 
