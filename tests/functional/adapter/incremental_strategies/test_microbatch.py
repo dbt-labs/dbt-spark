287531c482c96b1729d279311da71b4573df857c
Michelle Ark <michelle.ark@dbtlabs.com>
2024-09-24 23:18:27 +0100

    test using parquet

--- tests/functional/adapter/incremental_strategies/test_microbatch.py
+++ tests/functional/adapter/incremental_strategies/test_microbatch.py
@@ -6,7 +6,7 @@ from dbt.tests.adapter.incremental.test_incremental_microbatch import (
 
 # No requirement for a unique_id for spark microbatch!
 _microbatch_model_no_unique_id_sql = """
-{{ config(materialized='incremental', incremental_strategy='microbatch', event_time='event_time', batch_size='day', begin=modules.datetime.datetime(2020, 1, 1, 0, 0, 0), partition_by=['date_day']) }}
+{{ config(materialized='incremental', incremental_strategy='microbatch', event_time='event_time', batch_size='day', begin=modules.datetime.datetime(2020, 1, 1, 0, 0, 0), partition_by=['date_day'], file_format='parquet') }}
 select *, cast(event_time as date) as date_day
 from {{ ref('input_model') }}
 """
